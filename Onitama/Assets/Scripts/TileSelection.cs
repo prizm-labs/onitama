405f339c3dd7d9324f4696d29c3a483243db8365
Michael Garrido <michael.a.garrido@gmail.com>
2016-07-23 23:20:47 -0700

    initial gameboard

---
+++ Onitama/Assets/Scripts/TileSelection.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileSelection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetMouseButtonDown (0)){ 
+			RaycastHit hit; 
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+			if ( Physics.Raycast (ray,out hit,100.0f)) {
+
+
+
+
+				StartCoroutine(ScaleMe(hit.transform));
+				Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+
+				GameObject parent = hit.transform.parent.gameObject;
+
+				if (parent) {
+					Debug.Log("You selected the " + parent.name); // ensure you picked right object
+
+					if (parent.layer == 8) { // in TileGird layer
+						Vector3 worldPosition = parent.transform.position;
+
+						GameObject target = GameObject.Find ("SelectionTarget");
+						target.transform.position = new Vector3(worldPosition.x,worldPosition.y,target.transform.position.z);
+
+					}
+				}
+			}
+		}
+
+
+	}
+
+	IEnumerator ScaleMe(Transform objTr) {
+		objTr.localScale *= 1.2f;
+		yield return new WaitForSeconds(0.5f);
+		objTr.localScale /= 1.2f;
+	}
+}
