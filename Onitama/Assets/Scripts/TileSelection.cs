53c4787106cc3f687cfb79cb254e9990e8c370f8
Michael Garrido <michael.a.garrido@gmail.com>
2016-07-24 00:09:11 -0700

    selected pawn moves to selected tile

--- Onitama/Assets/Scripts/TileSelection.cs
+++ Onitama/Assets/Scripts/TileSelection.cs
@@ -3,41 +3,116 @@ using System.Collections;
 
 public class TileSelection : MonoBehaviour {
 
+	public GameObject selectedPawn;
+	public GameObject tileSelectionTarget;
+	public GameObject pawnSelectionTarget;
+	private bool IsPawnSelected = false; [SerializeField]
+
 	// Use this for initialization
 	void Start () {
 	
+		hideSelectionTargets ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if ( Input.GetMouseButtonDown (0)){ 
-			RaycastHit hit; 
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-			if ( Physics.Raycast (ray,out hit,100.0f)) {
+			
+
+
+			// if pawn not selected 
+			if (IsPawnSelected==false) {
+				selectPawn ();
+			} else {
+				selectTile ();
+			}
+
+			// if selected pawn
+
+
+		}
+
+
+	}
+
+	public void showPawnSelectionIndicator() {
+		pawnSelectionTarget.transform.GetChild(0).GetComponent<Renderer> ().enabled = true;
+	}
+
+	public void showTileSelectionIndicator() {
+		tileSelectionTarget.transform.GetChild(0).GetComponent<Renderer> ().enabled = true;
+	}
+
+	public void hideSelectionTargets() {
+		tileSelectionTarget.transform.GetChild(0).GetComponent<Renderer> ().enabled = false;
+		pawnSelectionTarget.transform.GetChild(0).GetComponent<Renderer> ().enabled = false;
+	}
+
+	public void selectPawn () {
+		RaycastHit hit; 
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+		var tileGridLayerMask = 1 << 9;
+
+		if (Physics.Raycast (ray, out hit, 100.0f, tileGridLayerMask)) {
+			GameObject parent = hit.transform.parent.gameObject;
+			Vector3 worldPosition = parent.transform.position;
+
+			pawnSelectionTarget.transform.position = new Vector3(worldPosition.x,worldPosition.y,pawnSelectionTarget.transform.position.z);
+
+			showPawnSelectionIndicator ();
+
+
+			selectedPawn = parent;
+			IsPawnSelected = true;
+		}
+	}
+
+	public void selectTile () {
+
+		RaycastHit hit; 
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+		var tileGridLayerMask = 1 << 8;
+
+		if ( Physics.Raycast (ray,out hit,100.0f,tileGridLayerMask)) {
 
 
 
 
-				StartCoroutine(ScaleMe(hit.transform));
-				Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+			StartCoroutine(ScaleMe(hit.transform));
+			Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
 
-				GameObject parent = hit.transform.parent.gameObject;
+			GameObject parent = hit.transform.parent.gameObject;
 
-				if (parent) {
-					Debug.Log("You selected the " + parent.name); // ensure you picked right object
+			if (parent) {
+				Debug.Log("You selected the " + parent.name); // ensure you picked right object
 
-					if (parent.layer == 8) { // in TileGird layer
-						Vector3 worldPosition = parent.transform.position;
 
-						GameObject target = GameObject.Find ("SelectionTarget");
-						target.transform.position = new Vector3(worldPosition.x,worldPosition.y,target.transform.position.z);
+				if (parent.layer == 8) { // in TileGird layer
+					Vector3 worldPosition = parent.transform.position;
 
-					}
+
+					tileSelectionTarget.transform.position = new Vector3(worldPosition.x,worldPosition.y,tileSelectionTarget.transform.position.z);
+					showTileSelectionIndicator ();
+
+					// automatically confirm tile selection
+					confirmTileSelection();
 				}
 			}
 		}
 
 
+	}
+
+
+	public void confirmTileSelection() {
+
+		hideSelectionTargets ();
+
+		selectedPawn.transform.position = new Vector3 (tileSelectionTarget.transform.position.x, tileSelectionTarget.transform.position.y, selectedPawn.transform.position.z);
+
+		selectedPawn = null;
+		IsPawnSelected = false;
 	}
 
 	IEnumerator ScaleMe(Transform objTr) {
